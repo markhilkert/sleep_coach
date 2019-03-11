4636bbd68ad485cf80a7cba7c28e1c1d67819687
markhilkert <mark.hilkert@gmail.com>
2019-03-11 15:33:20 -0500

    added nap controller actions

--- app/controllers/api/naps_controller.rb
+++ app/controllers/api/naps_controller.rb
@@ -1,2 +1,50 @@
 class Api::NapsController < ApplicationController
+
+  def index
+    @nap = Nap.all 
+    render 'index.json.jbuilder'
+  end
+
+  def create
+    @nap = Nap.new(
+                        sleep_id: params[:sleep_id],
+                        amount: params[:amount],
+                        time: params[:time],
+                        increased_impact: params[:increased_impact],
+                        type: "nap"
+                        )
+    
+    if @nap.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @nap.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @nap = Nap.find(params[:id])
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    @nap = Nap.find(params[:id])
+
+    @nap.sleep_id = params[:sleep_id] || @nap.sleep_id
+    @nap.amount = params[:amount] || @nap.amount
+    @nap.time = params[:time] || @nap.time
+    @nap.increased_impact = params[:increased_impact] || @nap.increased_impact
+
+    if @nap.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @nap.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    nap = Nap.find(params[:id])
+    nap.destroy
+    render json: {message: "Successfully removed morning sun exposure."}
+  end
+
 end
