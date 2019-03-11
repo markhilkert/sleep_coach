e01eb81ae1b1905c6b526404ace769aa23f59714
markhilkert <mark.hilkert@gmail.com>
2019-03-11 11:50:30 -0500

    migrated events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_164724) do
+ActiveRecord::Schema.define(version: 2019_03_11_164929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2019_03_11_164724) do
     t.decimal "amount"
     t.time "time"
     t.boolean "increased_impact"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.integer "sleep_id"
+    t.time "time"
+    t.decimal "duration"
+    t.string "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
