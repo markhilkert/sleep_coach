d2399c2be3ebcc92771eb8ae9a03eb2a3124f87e
markhilkert <mark.hilkert@gmail.com>
2019-03-11 14:29:12 -0500

    added all controllers

---
+++ app/controllers/api/alcohols_controller.rb
@@ -0,0 +1,2 @@
+class Api::AlcoholsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
+    get 'exercises/index'
+    get 'exercises/create'
+  end
+  namespace :api do
     post "/users" => "users#create"
 
     post "/sessions" => "sessions#create"
