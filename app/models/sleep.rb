c65ee9a6e7b0c3ffc3b47d8457d7e8907be33245
markhilkert <mark.hilkert@gmail.com>
2019-03-20 10:05:38 -0500

    updated associations to add has_one relationships

--- app/models/sleep.rb
+++ app/models/sleep.rb
@@ -3,13 +3,13 @@ class Sleep < ApplicationRecord
   has_many :digestions
   has_many :events
 
-  has_many :alcohols
-  has_many :caffeines
+  has_one :alcohol
+  has_one :caffeine
   has_many :exercises
-  has_many :late_meals
-  has_many :morning_suns
+  has_one :late_meal
+  has_one :morning_sun
   has_many :naps
-  has_many :relaxes
+  has_one :relax
 
   def time_in_bed
     time_in_seconds = self.end_time - self.start_time
