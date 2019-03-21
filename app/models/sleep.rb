6a29939bed88ccacb76f96828c305eafd58c4526
markhilkert <mark.hilkert@gmail.com>
2019-03-21 10:47:25 -0500

    added hours of sleep variable to sleep model and view

--- app/models/sleep.rb
+++ app/models/sleep.rb
@@ -11,7 +11,25 @@ class Sleep < ApplicationRecord
   has_many :naps
   has_one :relax
 
+  def friendly_sleep_time(time_in_seconds)
+    time_in_hours = time_in_seconds / 60 / 60.0
+    hours = time_in_hours.floor
+    minutes = ((time_in_hours - hours) * 60).floor
+    time_message = ""
+
+      time_message += "#{hours} #{'Hour'.pluralize(hours)}" if hours > 0
+      time_message += ", " if hours > 0 && minutes > 0 
+      time_message += "#{minutes} #{"Minute".pluralize(minutes)}" if minutes > 0
+      time_message += "no time" if time_message == ""
+
+    time_message
+  end
+
   def time_in_bed
     time_in_seconds = self.end_time - self.start_time
+  end
+
+  def hours_in_bed
+    time_in_bed / 60 / 60.0
   end
 end
