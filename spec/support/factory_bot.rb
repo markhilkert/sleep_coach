fe2b2a3426d6c89046690e1c9a584e882be404df
Mark Hilkert <armk@hey.com>
2022-02-26 10:25:14 -0600

    configure factory_bot

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,1 +1,7 @@
-puts "I AM LOADED"
+# FactoryBot Configuration file
+
+# Include FactoryBot methods so you don't have to
+# preface every single command with `FactoryBot.`
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+end
